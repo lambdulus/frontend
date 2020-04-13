3edb177b00d78440096729d7406482f99e8cd6cb
jalivert <184747279+jalivert@users.noreply.github.com>
2020-04-13 16:56:21 +0200

    clean a bit the App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import './App.css'
 
-import { AppState, BoxState } from './AppTypes'
+import { AppState, BoxState, Screen } from './AppTypes'
 
 import MenuBar from './components/MenuBar'
 import Box from './components/Box'
@@ -10,7 +10,6 @@ import { CreateBox } from './components/CreateBox'
 
 
 interface Props {}
-
 export default class App extends Component<Props, AppState> {
   constructor (props : Props) {
     super(props)
@@ -18,10 +17,13 @@ export default class App extends Component<Props, AppState> {
     this.state = {
       boxList : [],
       activeBoxIndex : 0,
+      currentScreen : Screen.MAIN,
     }
 
     this.insertBefore = this.insertBefore.bind(this)
+    this.setScreen = this.setScreen.bind(this)
 
+    // TODO: implement Class Keyboard Controller -> handling all keyboard events and firing events -> invoking handlers from this class
     // document.addEventListener('keydown', (event : KeyboardEvent) => {
     //   console.log(event)
     // })
@@ -31,22 +33,28 @@ export default class App extends Component<Props, AppState> {
   render () {
     return (
       <div id='app'>
-        <MenuBar state={ this.state } />
+        <MenuBar state={ this.state } onScreenChange={ this.setScreen } />
 
         <div className="mainSpace">
           <ul className="boxList UL">
             { this.state.boxList.map(
               (box : BoxState, i : number) =>
               <li className="LI" key={ box.__key }>
+                
+                {/* TODO: This will be refactored out to standalone component. */}
                 <div className='addBoxArea'>
                   { <CreateBox addNew={ (box : BoxState) => this.insertBefore(i, box) } /> }
                 </div>
+                
                 <Box state={ box } isActive={ i === this.state.activeBoxIndex } />
               </li>
             ) }
+
+            {/* TODO: This will be refactored out to standalone component. */}
             <div className='addBoxArea'>
               { <CreateBox addNew={ (box : BoxState) => this.insertBefore(this.state.boxList.length, box) } /> }
             </div>
+
           </ul>
         </div>
       </div>
@@ -54,7 +62,13 @@ export default class App extends Component<Props, AppState> {
   }
 
   insertBefore (index : number, box : BoxState) : void {
-    this.state.boxList.splice(index, 0, box)
-    this.setState({ boxList : this.state.boxList })
+    const { boxList } = this.state
+    
+    boxList.splice(index, 0, box)
+    this.setState({ boxList : boxList, activeBoxIndex : index })
+  }
+
+  setScreen (screen : Screen) : void {
+    this.setState({ currentScreen : screen })
   }
 }
