d8004a38035993ccbcc6cd00eaa9d2161a8b9509
Taskkill <johny95wil@gmail.com>
2020-05-01 19:03:51 +0200

    implement base for whitelisting specific BoxTypes inside Notebook

--- src/components/CreateBox.tsx
+++ src/components/CreateBox.tsx
@@ -1,21 +1,45 @@
 import React from 'react'
 
-import { BoxState, BoxType } from '../AppTypes'
-import { createNewUntypedLambda} from '../untyped-lambda-integration/AppTypes'
+import { BoxState, BoxType, BoxesWhitelist, ANY_BOX, NO_BOX } from '../AppTypes'
+import { createNewUntypedLambda, ADD_BOX_LABEL } from '../untyped-lambda-integration/AppTypes'
 
 
-interface Props { addNew : (box : BoxState) => void }
+interface Props {
+  addNew : (box : BoxState) => void,
+  whiteList : BoxesWhitelist,
+}
+
+function anyBoxAllowed (whitelist : BoxesWhitelist) : boolean {
+  return whitelist === ANY_BOX
+}
+
+function noBoxAllowed (whitelist : BoxesWhitelist) : boolean {
+  return whitelist === NO_BOX
+}
+
+function isAllowed (type : BoxType, whitelist : BoxesWhitelist) : boolean {
+  console.log(whitelist)
+  return anyBoxAllowed(whitelist) || (whitelist as Array<BoxType>).includes(type)
+}
 
 export function CreateBox (props : Props) : JSX.Element {
-  return (
-    <div className='addButtons'>
+  const { addNew, whiteList } : Props = props
+
+  const addLambdaBoxIfAllowed = (allowed : boolean) => (
+    allowed ?
       <p
         className='plusBtn'
         title='Create new λ box'
         onClick={ () => props.addNew(createNewUntypedLambda()) } // TODO: some imported function from the Integration -- like Integration.CreateNewBox()
       >
-        <i>+ λ</i>
+        <i>{ ADD_BOX_LABEL }</i>
       </p>
+      :
+      null
+  )
+
+  const addLispBoxIfAllowed = (allowed : boolean) => (
+    allowed ?
       <p
         className='plusBtn'
         title='Create new Lisp box'
@@ -23,13 +47,33 @@ export function CreateBox (props : Props) : JSX.Element {
       >
         <i>+ Lisp</i>
       </p>
-      <p
-        className='plusBtn'
-        title='Create new MarkDown box'
-        onClick={ () => props.addNew({type : BoxType.UNTYPED_LAMBDA, __key : Date.now()}) }
-      >
-        <i>+ MD</i>
-      </p>
-    </div>
+      :
+      null
+  )
+
+  const addMDBoxIfAllowed = (allowed : boolean) => (
+    allowed ?
+    <p
+      className='plusBtn'
+      title='Create new MarkDown box'
+      onClick={ () => props.addNew({type : BoxType.UNTYPED_LAMBDA, __key : Date.now()}) }
+    >
+      <i>+ MD</i>
+    </p>
+    :
+    null
+  )
+
+  return (
+    noBoxAllowed(whiteList) ?
+      null as any
+      :
+      <div className='addBoxArea'>
+        <div className='addButtons'>
+          { addLambdaBoxIfAllowed(isAllowed (BoxType.UNTYPED_LAMBDA, whiteList)) }
+          { addLispBoxIfAllowed(isAllowed(BoxType.LISP, whiteList)) }
+          { addMDBoxIfAllowed(isAllowed(BoxType.MARKDOWN, whiteList)) }
+        </div>
+      </div>
   )
 }
