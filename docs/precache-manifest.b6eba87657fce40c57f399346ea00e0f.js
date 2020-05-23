d65e1d7dfaa7f35c22674898c248745e6546d99e
Taskkill <johny95wil@gmail.com>
2020-05-23 19:37:45 +0200

    prepare building to docs folder

---
+++ docs/precache-manifest.b6eba87657fce40c57f399346ea00e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c333b77442d839f43fdc94aed9e89a",
+    "url": "/new-frontend/index.html"
+  },
+  {
+    "revision": "af8bc7b8ca4d88bfb959",
+    "url": "/new-frontend/static/css/2.67f14537.chunk.css"
+  },
+  {
+    "revision": "9e8e33c80df1893547f1",
+    "url": "/new-frontend/static/css/main.5718e590.chunk.css"
+  },
+  {
+    "revision": "af8bc7b8ca4d88bfb959",
+    "url": "/new-frontend/static/js/2.3b9ea30c.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/new-frontend/static/js/2.3b9ea30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8e33c80df1893547f1",
+    "url": "/new-frontend/static/js/main.19d82151.chunk.js"
+  },
+  {
+    "revision": "98f1bd22275a2a2f2f5c",
+    "url": "/new-frontend/static/js/runtime-main.6e0f890c.js"
+  }
+]);
