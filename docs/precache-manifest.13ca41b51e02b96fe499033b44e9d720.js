5964098b2e4305bd1eb2bb4341f87b0635445a17
Taskkill <johny95wil@gmail.com>
2020-05-23 23:55:37 +0200

    deploy experimental side-bar

--- docs/precache-manifest.13ca41b51e02b96fe499033b44e9d720.js
+++ docs/precache-manifest.13ca41b51e02b96fe499033b44e9d720.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16c333b77442d839f43fdc94aed9e89a",
+    "revision": "d467496c440b87b1dd8e150b37dac610",
     "url": "/new-frontend/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/new-frontend/static/css/2.67f14537.chunk.css"
   },
   {
-    "revision": "9e8e33c80df1893547f1",
-    "url": "/new-frontend/static/css/main.5718e590.chunk.css"
+    "revision": "34a72681c676ffe1c264",
+    "url": "/new-frontend/static/css/main.c0b001c3.chunk.css"
   },
   {
     "revision": "af8bc7b8ca4d88bfb959",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/new-frontend/static/js/2.3b9ea30c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9e8e33c80df1893547f1",
-    "url": "/new-frontend/static/js/main.19d82151.chunk.js"
+    "revision": "34a72681c676ffe1c264",
+    "url": "/new-frontend/static/js/main.5dce5ca1.chunk.js"
   },
   {
     "revision": "98f1bd22275a2a2f2f5c",
