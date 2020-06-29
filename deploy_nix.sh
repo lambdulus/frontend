5746cf865e9de37961d39e9e5f7ebcd57a2bfe10
jalivert <184747279+jalivert@users.noreply.github.com>
2020-06-29 22:33:41 +0200

    fix build script bugs and errors

--- deploy_nix.sh
+++ deploy_nix.sh
@@ -7,7 +7,7 @@ cp package.json package.old.json
 BUILD_VERSION="$(date +'%Y')-$(date +'%m')-$(date +'%d')"
 
 # replaces placeholder in homepage field with current build-version
-sed -i "s/@BUILD-VERSION/$BUILD_VERSION/g" package.json
+sed -i "s/@BUILD-VERSION/build\/$BUILD_VERSION/g" package.json
 
 # just to be sure
 rm -rf build*
@@ -15,14 +15,20 @@ rm -rf build*
 # run react-scripts build
 npm run build
 
-# move the built app to the versioned sub-dir
-mv build/* "docs/build/$BUILD_VERSION/"
+# create versioned directory in the build dir
+mkdir "docs/build/$BUILD_VERSION"
+
+# copy the built app to the versioned sub-dir
+cp -rp build/* "docs/build/$BUILD_VERSION/"
 
 # clean behind myself
+rm -rf build/*
 rm package.json
 mv package.old.json package.json
 
-
+echo ""
+echo "Half-way through! Hold on!"
+echo ""
 
 # now to build to the root url for easier acces to the latest build
 
@@ -38,13 +44,11 @@ rm -rf build*
 # run react-scripts build
 npm run build
 
-# removes all files of latest build but leaves all the older versions intact
-rm -rf docs/!("build")
-
-# move the built app to the root of the docs directory
-mv build/* docs/
+# copy the built app to the root of the docs directory
+cp -rp build/* docs/
 
 # clean behind myself
+rm -rf build/*
 rm package.json
 mv package.old.json package.json
 
@@ -52,3 +56,9 @@ mv package.old.json package.json
 echo ""
 echo "BUILD WENT OK - EVERYTHING DONE"
 echo ""
+
+
+# finally - commit and push
+echo "Commiting and Pushing to the Upstream"
+git commit -m "deploy build ver. $BUILD_VERSION"
+git push
