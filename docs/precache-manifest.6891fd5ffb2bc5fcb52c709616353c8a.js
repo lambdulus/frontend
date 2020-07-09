2c722ef46f80f92f5f05fb967d8cbff7f04d9c55
Taskkill <johny95wil@gmail.com>
2020-07-09 23:43:21 +0200

    deploy build ver. 2020-07-09

---
+++ docs/precache-manifest.6891fd5ffb2bc5fcb52c709616353c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f61bd4212f8f5c5eb18b1541d4f5c90",
+    "url": "/new-frontend/index.html"
+  },
+  {
+    "revision": "b7ff901e9b6e2096e9af",
+    "url": "/new-frontend/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "367e5ff7b4487e01925b",
+    "url": "/new-frontend/static/css/main.a8b13253.chunk.css"
+  },
+  {
+    "revision": "b7ff901e9b6e2096e9af",
+    "url": "/new-frontend/static/js/2.c59eab5b.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/new-frontend/static/js/2.c59eab5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367e5ff7b4487e01925b",
+    "url": "/new-frontend/static/js/main.82e68b3c.chunk.js"
+  },
+  {
+    "revision": "98f1bd22275a2a2f2f5c",
+    "url": "/new-frontend/static/js/runtime-main.6e0f890c.js"
+  }
+]);
