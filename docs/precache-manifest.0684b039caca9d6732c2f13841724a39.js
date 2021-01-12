60244a30a1d609534ee289f27da28dcae96bcb71
Taskkill <johny95wil@gmail.com>
2021-01-12 22:18:02 +0100

    deploy build ver. 2021-01-12

---
+++ docs/precache-manifest.0684b039caca9d6732c2f13841724a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965c8f0b45a67190fd564d246782f564",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "2eca66460727fc628a8b",
+    "url": "/frontend/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "048762bebea490b126ca",
+    "url": "/frontend/static/css/main.6df6fd0c.chunk.css"
+  },
+  {
+    "revision": "2eca66460727fc628a8b",
+    "url": "/frontend/static/js/2.91d32d2e.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/frontend/static/js/2.91d32d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048762bebea490b126ca",
+    "url": "/frontend/static/js/main.242e2be0.chunk.js"
+  },
+  {
+    "revision": "b96ce556996e7ee00db8",
+    "url": "/frontend/static/js/runtime-main.91605631.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/new-frontend/precache-manifest.671253499178184d13b79444a1adb715.js"
+  "/frontend/precache-manifest.0684b039caca9d6732c2f13841724a39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
