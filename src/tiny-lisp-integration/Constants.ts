e598c0ffa3bc9599c1799771c95c0c1a99ba6555
jalivert <184747279+jalivert@users.noreply.github.com>
2021-10-16 16:11:51 +0200

    rebasing on the develop - "start working on the lisp integration"

---
+++ src/tiny-lisp-integration/Constants.ts
@@ -0,0 +1,30 @@
+
+import { BoxType } from "../Types";
+import { TinyLispSettings, TinyLispState } from "./Types"
+
+
+export const ADD_BOX_LABEL = '+ Lisp Expression'
+
+export const CODE_NAME = 'TINY_LISP'
+
+
+export function createNewTinyLispExpression (defaultSettings : TinyLispSettings) : TinyLispState {
+  return {
+    __key : Date.now().toString(),
+    type : BoxType.LISP,
+    title : 'New Tiny Lisp Expression',
+    minimized : false,
+    menuOpen : false,
+    settingsOpen : false,
+
+
+    expression : '',
+
+    editor : {
+      placeholder : '',
+      content : '',
+      caretPosition : -1,
+      syntaxError : null,
+    }
+  };
+}
